1008ab27072d1f1ea9b6573b3e849b63594a622a
Lucas Vinicius <lucasvini269@gmail.com>
2025-10-22 20:33:27 -0300

    feat(laravel): inicia o projeto

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Zema - Entrar</title>
+    </head>
+    <body>
+        <h1>Bem-vindo à Zema!</h1>
+    </body>
+</html>
