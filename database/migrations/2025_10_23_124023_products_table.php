4127e148065cc24f2f07e94d87cf8254c2c0a165
Lucas Vinicius <lucasvini269@gmail.com>
2025-10-24 11:19:26 -0300

    refactor(product): refatora o código

--- database/migrations/2025_10_23_124023_products_table.php
+++ database/migrations/2025_10_23_124023_products_table.php
@@ -11,7 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        
+        Schema::firsOrCreate('products', function (Blueprint $table) {
             $table->id();
             $table->string('store');
             $table->string('description');
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -13,28 +13,28 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        ProductModel::create([
+        ProductModel::firstOrCreate([
             'store' => 'Casas Freire',
             'description' => 'Playstation 5 Pro',
             'flat' => 'Portáteis(reparo)',
             'months_guarantee' => 12
         ]);
 
-        ProductModel::create([
+        ProductModel::firstOrCreate([
             'store' => 'Móveis Gazin',
             'description' => 'Playstation 4 Pro',
             'flat' => 'Portáteis(reparo)',
             'months_guarantee' => 12
         ]);
 
-        ProductModel::create([
+        ProductModel::firstOrCreate([
             'store' => 'Lojas Valdir II',
             'description' => 'Xbox One',
             'flat' => 'Portáteis(reparo)',
             'months_guarantee' => 12
         ]);
 
-        ProductModel::create([
+        ProductModel::firstOrCreate([
             'store' => 'Lojas Valdir',
             'description' => 'Xbox Series X',
             'flat' => 'Portáteis(reparo)',
