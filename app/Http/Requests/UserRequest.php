a725cc99ea29a2895e93d66ab8bd26347b6b09e5
Lucas Vinicius <lucasvini269@gmail.com>
2025-10-30 00:10:21 -0300

    feat(validação de formulário): garante que o usuário pode editar colunas unique do próprio registro

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -6,29 +6,24 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class UserRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
+        // Captura o parâmetro da rota, ou seja, se a rota possuir "/{user}" então captura o ID do registro, se não possuir parâmetro (ex.: em formulários post - cadastrar) retorna null
+        $user = $this->route('user');
+        
         return [
             'name' => 'sometimes|required|min:3',
-            'cpf' => 'sometimes|required',
+            'cpf' => 'sometimes|required|unique:users,cpf,' . ($user ? $user->id : null),
             'date_birth' => 'sometimes|required',
             'gender' => 'sometimes|required',
-            'email' => 'sometimes|required',
-            'telephone' => 'sometimes|required',
-            'password' => 'sometimes|required',
+            'email' => 'sometimes|required|email|unique:users,email,' . ($user ? $user->id : null),
+            'telephone' => 'sometimes|required|unique:users,telephone,' . ($user ? $user->id : null),
+            'password' => 'sometimes|required|min:6'
         ];
     }
 }
