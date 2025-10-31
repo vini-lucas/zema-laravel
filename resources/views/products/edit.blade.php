6a630ce4d6fa77c59224fdbbe209e9898a25b529
Lucas Vinicius <lucasvini269@gmail.com>
2025-10-31 17:46:59 -0300

    feat(products): corrigir exibição de opções no formulário de edição de produtos

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -14,25 +14,31 @@
         <label for="store">Loja:</label>
         <select name="store" id="store">
             @foreach ($enterprises as $enterprise)
-                <option value="{{ $enterprise }}" {{ ($enterprise == $product->store) ? 'selected' : '' }}>$enterprise</option>
+                <option value="{{ $enterprise->name }}" {{ ($enterprise->name == $product->store) ? 'selected' : '' }}>
+                    {{ $enterprise->name }}</option>
             @endforeach
         </select><br><br>
 
         <label for="description">Descrição:</label>
-        <input type="text" name="description" id="description" placeholder="Nome, tipo, marca, cor e voltagem do produto" value="{{ $product->description }}"><br><br>
+        <input type="text" name="description" id="description" placeholder="Nome, tipo, marca, cor e voltagem do produto"
+            value="{{ $product->description }}"><br><br>
 
-        <label for="flat">Plano:</label><span style="color: #f00">Criar tabela para isto posteriormente*</span>
+        <label for="flat">Plano:</label>
         <select name="flat" id="flat">
-            <option value="Móveis" {{ ($product->flat == 'Móveis') ? 'selected' : '' }}>Móveis</option>
-            <option value="Portáteis - troca" {{ ($product->flat == 'Portáteis - troca') ? 'selected' : '' }}>Portáteis - troca</option>
-            <option value="Portáteis - reparo" {{ ($product->flat == 'Portáteis - reparo') ? 'selected' : '' }}>Portáteis - reparo</option>
-        </select><br><br> 
+            <option value="Móveis" {{ $product->flat == 'Móveis' ? 'selected' : '' }}>Móveis</option>
+            <option value="Portáteis - troca" {{ $product->flat == 'Portáteis - troca' ? 'selected' : '' }}>Portáteis -
+                troca</option>
+            <option value="Portáteis - reparo" {{ $product->flat == 'Portáteis - reparo' ? 'selected' : '' }}>Portáteis -
+                reparo</option>
+        </select> - <span style="color: #f00">Criar tabela para isto posteriormente*</span><br><br>
 
         <label for="months_guarantee">Garantia de fábrica:</label>
-        <input type="text" name="months_guarantee" id="months_guarantee" placeholder="Tempo em meses" value="{{ $product->months_guarantee }}"><br><br>
+        <input type="text" name="months_guarantee" id="months_guarantee" placeholder="Tempo em meses"
+            value="{{ $product->months_guarantee }}"><br><br>
 
         <label for="factory_price">Preço de fábrica:</label>
-        <input type="text" name="factory_price" id="factory_price" placeholder="Ex.: R$500,00" value="{{ $product->factory_price }}"><br><br>
+        <input type="text" name="factory_price" id="factory_price" placeholder="Ex.: R$500,00"
+            value="{{ $product->factory_price }}"><br><br>
 
         <button type="submit">Salvar</button> - <a href="{{ route('products.index') }}">Voltar</a>
 
