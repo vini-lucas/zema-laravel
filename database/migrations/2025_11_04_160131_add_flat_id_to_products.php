bccaccb9004150309bd8d9794c5b64b1b8f86d08
Lucas Vinicius <lucasvini269@gmail.com>
2025-11-04 17:45:17 -0300

    feat(migration): adicionar coluna flat_id na tabela products com restrições de chave estrangeira

---
+++ database/migrations/2025_11_04_160131_add_flat_id_to_products.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->foreignId('flat_id')
+            ->after('months_guarantee')
+            ->default(1)
+            ->constrained('flats')
+            ->onUpdate('cascade')
+            ->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropForeign(['flat_id']);
+            $table->dropColumn('flat_id');
+        });
+    }
+};
