4eb169157fa2803c9822d40acf7b608e8a0df300
Lucas Vinicius <lucasvini269@gmail.com>
2025-11-04 11:27:50 -0300

    feat(flat): implementar interface Auditable no modelo Flat feat(config): atualizar fuso horário da aplicação para America/Sao_Paulo

--- app/Models/Flat.php
+++ app/Models/Flat.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use OwenIt\Auditing\Contracts\Auditable;
 
-class Flat extends Model
+class Flat extends Model implements Auditable
 {
     /** @use HasFactory<\Database\Factories\FlatFactory> */
     use HasFactory;
