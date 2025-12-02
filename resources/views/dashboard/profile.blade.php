313e110f00d1575267d3c20eb29806ae9dbd7eba
Lucas Vinicius <lucasvini269@gmail.com>
2025-12-02 11:16:24 -0300

    fix: corrige pequenas imperfeições dos CRUDs do sistema

--- resources/views/dashboard/profile.blade.php
+++ resources/views/dashboard/profile.blade.php
@@ -6,8 +6,8 @@
 
     <span><strong>Nome: </strong></span> <span>{{ $user->name }}</span><br>
     <span><strong>CPF: </strong></span> <span>{{ $user->cpf }}</span><br>
-    <span><strong>Nascimento: </strong></span> <span>{{ $user->date_birth }}</span><br>
-    <span><strong>Gênero: </strong></span> <span>{{ $user->gender }}</span><br>
+    <span><strong>Nascimento: </strong></span> <span>{{ \Carbon\Carbon::parse($user->date_birth)->format('d/m/Y') . ' - ' . date('Y') - \Carbon\Carbon::parse($user->date_birth)->format('Y') . ' anos' }}</span><br>
+    <span><strong>Gênero: </strong></span> <span>{{ ucfirst($user->gender) == 'Não_informado' ? 'Não informado' : ucfirst($user->gender) }}</span><br>
     <span><strong>E-mail: </strong></span> <span>{{ $user->email }}</span><br>
     <span><strong>Telefone: </strong></span> <span>{{ $user->telephone }}</span><br>
     <span><strong>Empresa - Filial: </strong></span> <span>{{ $enterprise->name . ' - ' . $branch->city }}</span><br>
