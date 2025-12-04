4dec00c01eed561c08a5e6cdbc0d2e0d95f18303
Lucas Vinicius <lucasvini269@gmail.com>
2025-12-04 17:05:07 -0300

    refactor: apresenta a idade exata do usuário na visualização detalhada da página

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -10,7 +10,7 @@
 
     <span>Nome: {{ $user->name }}</span><br>
     <span>CPF: {{ $cpf }}</span><br>
-    <span>Nascimento: <span>{{ \Carbon\Carbon::parse($user->date_birth)->format('d/m/Y') . ' - ' . date('Y') - \Carbon\Carbon::parse($user->date_birth)->format('Y') . ' anos' }} </span><br>
+    <span>Nascimento: <span>{{ \Carbon\Carbon::parse($user->date_birth)->format('d/m/Y') . ' - ' . date('Y') - \Carbon\Carbon::parse($user->date_birth)->format('Y') . ' anos, ' . 12 - \Carbon\Carbon::parse($user->date_birth)->format('m') . ' meses e ' . date('d') . ' dias' }} </span><br>
     <span>Gênero: {{ ($user->gender == 'masculino') ? 'Masculino' : (($user->gender == 'feminino') ? 'Feminino': 'Não informado') }}</span><br>
     <span>E-mail: {{ $user->email }}</span><br>
     <span>Telefone: {{ $telephone }}</span><br>
