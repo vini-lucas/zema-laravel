1eaa39619df1313af74db92d7676a35f4a085c0c
Lucas Vinicius <lucasvini269@gmail.com>
2025-12-05 10:38:37 -0300

    refactor: altera o cpf e telefone na visualização do perfil para deixá-lo mais amigável

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -24,6 +24,10 @@ class DashboardController extends Controller
         $branch = Branch::where('id', $user->branch_id)->first();
         $enterprise = Enterprise::where('id', $branch->enterprise_id)->first();
         $access = LevelAccess::where('id', $user->level_access_id)->first();
-        return view('dashboard.profile', ['user' => $user, 'branch' => $branch, 'enterprise' => $enterprise, 'access' => $access]);
+        $cpf_array = str_split($user->cpf);
+        $cpf = $cpf_array[0] . $cpf_array[1] . $cpf_array[2] . '.' . $cpf_array[3] . $cpf_array[4] . $cpf_array[5] . '.' . $cpf_array[6] . $cpf_array[7] . $cpf_array[8] . '-' . $cpf_array[9] . $cpf_array[10];
+        $telephone_array = str_split($user->telephone);
+        $telephone = '(' . $telephone_array[0] . $telephone_array[1] . ')' . ' ' . $telephone_array[2] . ' ' . $telephone_array[3] . $telephone_array[4] . $telephone_array[5] . $telephone_array[6] . ' - ' . $telephone_array[7] . $telephone_array[8] . $telephone_array[9] . $telephone_array[10];
+        return view('dashboard.profile', ['user' => $user, 'branch' => $branch, 'enterprise' => $enterprise, 'access' => $access, 'cpf' => $cpf, 'telephone' => $telephone]);
     }
 }
--- resources/views/dashboard/profile.blade.php
+++ resources/views/dashboard/profile.blade.php
@@ -5,11 +5,11 @@
     <h2>Perfil</h2>
 
     <span><strong>Nome: </strong></span> <span>{{ $user->name }}</span><br>
-    <span><strong>CPF: </strong></span> <span>{{ $user->cpf }}</span><br>
-    <span><strong>Nascimento: </strong></span> <span>{{ \Carbon\Carbon::parse($user->date_birth)->format('d/m/Y') . ' - ' . date('Y') - \Carbon\Carbon::parse($user->date_birth)->format('Y') . ' anos' }}</span><br>
+    <span><strong>CPF: </strong></span> <span>{{ $cpf }}</span><br>
+    <span><strong>Nascimento: </strong></span> <span>{{ \Carbon\Carbon::parse($user->date_birth)->format('d/m/Y') . ' - ' . date('Y') - \Carbon\Carbon::parse($user->date_birth)->format('Y') . ' anos, ' . 12 - \Carbon\Carbon::parse($user->date_birth)->format('m') . ' meses e ' . date('d') . ' dias' }}</span><br>
     <span><strong>Gênero: </strong></span> <span>{{ ucfirst($user->gender) == 'Não_informado' ? 'Não informado' : ucfirst($user->gender) }}</span><br>
     <span><strong>E-mail: </strong></span> <span>{{ $user->email }}</span><br>
-    <span><strong>Telefone: </strong></span> <span>{{ $user->telephone }}</span><br>
+    <span><strong>Telefone: </strong></span> <span>{{ $telephone }}</span><br>
     <span><strong>Empresa - Filial: </strong></span> <span>{{ $enterprise->name . ' - ' . $branch->city }}</span><br>
     <span><strong>Acesso: </strong></span> <span>{{ $access->name }}</span><br>
     <span><strong>Foto: </strong></span> <span>Sem foto por ora</span><br><br>
