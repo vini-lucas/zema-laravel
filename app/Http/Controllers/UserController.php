b45b69775cadc67a19c36f79883261d21af96f1a
Lucas Vinicius <lucasvini269@gmail.com>
2025-12-30 15:45:48 -0300

    refactor: retira uma biblioteca que não estava sendo utilizada na respectiva controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,7 +16,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
-use Spatie\Permission\Models\Role;
 
 class UserController extends Controller
 {
