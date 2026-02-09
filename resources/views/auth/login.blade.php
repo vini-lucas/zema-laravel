87cc7df9fa699b321727c98284671c0f6a2d7890
Lucas Vinicius <lucasvini269@gmail.com>
2026-02-09 17:28:40 -0300

    feat: implementando front da plataforma

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,19 +1,34 @@
 @extends('layouts.login')
 
 @section('content')
+    <form action="{{ route('login.proccess') }}" method="POST">
+        @csrf
+        @method('POST')
 
-    <div class="w-full sm:w-150 h-90 bg-[#DCDCDC] rounded-md flex items-center justify-center">
-        <div class="bg-amber-500 h-40 w-full flex items-center justify-center flex-col gap-2">
+        <div class="w-full sm:w-150 h-90 bg-[#DCDCDC] rounded-md flex items-center justify-center">
+            <div class="bg-amber-500 h-45 w-full flex items-center justify-center flex-col gap-2">
 
-            <input type="text" name="cpf" id="cpf" placeholder="XXX.XXX.XXX-XX" value="{{ old('cpf') }}" class="w-[70%] h-10 bg-[#C0C0C0] border-3 border-[#808080] rounded-sm text-center text-[#4F4F4F] font-semibold focus:outline-none focus:shadow-[0_0_15px_rgba(0,0,0,0.15)] focus:shadow-black/30" autocomplete="off">
+                <input type="text" name="cpf" id="cpf" placeholder="XXX.XXX.XXX-XX" value="{{ old('cpf') }}"
+                    class="w-[50%] h-8 bg-[#C0C0C0] border-2 border-[#808080] rounded-sm text-center text-[#4F4F4F] font-semibold focus:outline-none focus:shadow-[0_0_15px_rgba(0,0,0,0.15)] focus:shadow-black/30"
+                    autocomplete="off">
 
-            <input type="password" name="password" id="password" placeholder="************" value="{{ old('password') }}" class="w-[70%] h-10 bg-[#C0C0C0] border-3 border-[#808080] rounded-sm text-center text-[#4F4F4F] font-bold focus:outline-none focus:shadow-[0_0_15px_rgba(0,0,0,0.15)] focus:shadow-black/30 text-lg" autocomplete="off">
+                <input type="password" name="password" id="password" placeholder="************"
+                    value="{{ old('password') }}"
+                    class="w-[50%] h-8 bg-[#C0C0C0] border-2 border-[#808080] rounded-sm text-center text-[#4F4F4F] font-bold focus:outline-none focus:shadow-[0_0_15px_rgba(0,0,0,0.15)] focus:shadow-black/30 text-lg"
+                    autocomplete="off">
 
-            <div>
+                <div class="bg-emerald-700 w-full h-10 -mb-15 flex justify-between">
+                    <div>
+                        <input type="submit" value="Entrar" class="">
+                    </div>
+
+                </div>
+
             </div>
-
         </div>
-    </div>
+
+
+    </form>
     {{-- <h2>Conecte-se!</h2>
 
     <x-alert />
