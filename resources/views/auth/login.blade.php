9ca9a3639bdb50f53ed1a44f53cddd0960cd3879
Lucas Vinicius <lucasvini269@gmail.com>
2026-02-10 11:27:28 -0300

    feat: implementando front da plataforma

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,23 +5,35 @@
         @csrf
         @method('POST')
 
-        <div class="w-full sm:w-150 h-90 bg-[#DCDCDC] rounded-md flex items-center justify-center">
-            <div class="bg-amber-500 h-45 w-full flex items-center justify-center flex-col gap-2">
+        <div
+            class="w-full sm:w-150 h-60 bg-[#DCDCDC] rounded-md flex items-center justify-center shadow-[0_0_15px_rgba(0,0,0,0.15)] shadow-black/30">
+            <div class="h-45 w-full flex items-center justify-center flex-col gap-4">
 
                 <input type="text" name="cpf" id="cpf" placeholder="XXX.XXX.XXX-XX" value="{{ old('cpf') }}"
-                    class="w-[50%] h-8 bg-[#C0C0C0] border-2 border-[#808080] rounded-sm text-center text-[#4F4F4F] font-semibold focus:outline-none focus:shadow-[0_0_15px_rgba(0,0,0,0.15)] focus:shadow-black/30"
+                    class="w-[80%] h-8 bg-[#C0C0C0] border-2 border-[#808080] rounded-sm text-center text-[#4F4F4F] font-semibold focus:outline-none focus:shadow-[0_0_15px_rgba(0,0,0,0.15)] focus:shadow-black/30 cursor-pointer"
                     autocomplete="off">
 
                 <input type="password" name="password" id="password" placeholder="************"
                     value="{{ old('password') }}"
-                    class="w-[50%] h-8 bg-[#C0C0C0] border-2 border-[#808080] rounded-sm text-center text-[#4F4F4F] font-bold focus:outline-none focus:shadow-[0_0_15px_rgba(0,0,0,0.15)] focus:shadow-black/30 text-lg"
+                    class="w-[80%] h-8 bg-[#C0C0C0] border-2 border-[#808080] rounded-sm text-center text-[#4F4F4F] font-bold focus:outline-none focus:shadow-[0_0_15px_rgba(0,0,0,0.15)] focus:shadow-black/30 text-lg cursor-pointer mb-5"
                     autocomplete="off">
 
-                <div class="bg-emerald-700 w-full h-10 -mb-15 flex justify-between">
-                    <div>
-                        <input type="submit" value="Entrar" class="">
+                <div class="w-full h-10 -mb-8 flex justify-center sm:justify-between px-2">
+                    <button type="submit"
+                        class="bg-[#32CD32] border-3 border-[#228B22] text-[#008000] p-4 rounded-sm text-sm font-bold flex items-center justify-center cursor-pointer w-70 hover:bg-[#228B22] hover:text-[#32CD32] transition-all duration-300 ease-in-out gap-1">
+                        Entrar
+                        <i class="fa-solid fa-arrow-right-to-bracket text-lg"></i>
+                    </button>
+                    <div class="inline-flex gap-2">
+                        <button type="button" class="bg-red-400 border-3 border-red-700 text-red-800 p-4 rounded-sm text-sm font-bold flex items-center justify-center cursor-pointer w-35 hover:bg-red-700 hover:text-red-400 transition-all duration-300 ease-in-out gap-1">
+                            Esqueceu?
+                            <i class="fa-solid fa-user-pen"></i>
+                        </button>
+                        <button type="button" class="bg-blue-400 border-3 border-blue-700 text-blue-800 p-4 rounded-sm text-sm font-bold flex items-center justify-center cursor-pointer w-35 hover:bg-blue-700 hover:text-blue-400 transition-all duration-300 ease-in-out gap-1">
+                            Sou NOVO!
+                        <i class="fa-solid fa-user-plus"></i>
+                    </button>
                     </div>
-
                 </div>
 
             </div>
--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Zema - Entrar</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-linear-to-l from-[#191970] to-[#3030D6] h-screen flex justify-center items-center">
